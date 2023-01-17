125ef2228455e6e32f64c2cfbaed2a160425db8b
Claire Lillie <97633650+clairelillie@users.noreply.github.com>
2023-01-17 11:10:47 -0500

    -

---
+++ 0-hello-world/hello.c
@@ -0,0 +1,11 @@
+// PID: 730476615
+// I pledge the COMP 211 honor code.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    printf("Hello, world.\n");
+    printf("Welcome to C!\n");
+    return EXIT_SUCCESS;
+}
